a3bbf8204ce7783c21b62d9f55e9531cf868fee7
Thomas Machin <thomas.machin@fujitsu.com>
2025-12-13 18:34:20 +0000

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 import pytest
-from app import app as flask_app
+import os
+import sys 
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, PROJECT_ROOT)
+import app as flask_app
 
 
 @pytest.fixture
